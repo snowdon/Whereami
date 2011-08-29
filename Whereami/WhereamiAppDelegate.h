c23ce89539f07189f9d228d0a11db03cb74be002
Xiaochun Shen <snowdonshen@gmail.com>
2011-08-29 14:29:53 +0800

    Initial Commit

---
+++ Whereami/WhereamiAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by  Chuns on 11-8-29.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
